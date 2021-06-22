98dd9a534b833e7bbded73b11543539b4fb07009
kamal-kaur04 <kmlkaur73@gmail.com>
2021-06-22 14:18:46 +0530

    Added Filter Expenses Feature

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
-//import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-//import ExpenseItem from './components/Expenses/ExpenseItem.js';
 import './components/Expenses/Expenses.css';
 import Expenses from './components/Expenses/Expenses.js'
 import NewExpense from './components/NewExpense/NewExpense.js'
 
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: 'e2',
+    title: 'New TV',
+    amount: 799.49,
+    date: new Date(2021, 2, 12)
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: 'e2',
-      title: 'New TV',
-      amount: 799.49,
-      date: new Date(2021, 2, 12)
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   // return React.createElement(
   //   'div',
   //   {},
@@ -40,10 +41,18 @@ const App = () => {
   //   React.createElement(Expenses, {items: expenses})
   // );
 
+  const addExpenseHandler = expense => {
+    // console.log('In App.js');
+    // console.log(expense);
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
   return (
     <div>
-      <NewExpense />
-      <Expenses items={expenses}/>
+      <NewExpense onAddExpense = {addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+//import React, {useState} from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css'
@@ -6,16 +6,16 @@ import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
 
-  const [title, setTitle] = useState(props.title); //Array Destructuring - to store bot elements in separate variables or constants
-  console.log('ExpenseItem');
+  //const [title, setTitle] = useState(props.title); //Array Destructuring - to store bot elements in separate variables or constants
+  //console.log('ExpenseItem');
 
   //let title = props.title;
 
-  const clickHandler = () => {
-    //title = 'Updated';
-    setTitle('Updated');
-    console.log(title);
-  };
+  // const clickHandler = () => {
+  //   //title = 'Updated';
+  //   setTitle('Updated');
+  //   //console.log(title);
+  // };
   // const expenseDate = new Date(2021, 2, 28);
   // const expenseTitle = 'Car Insurance';
   // const expenseAmount = 294.67;
@@ -28,10 +28,9 @@ const ExpenseItem = (props) => {
     <Card className = 'expense-item'>
       <ExpenseDate date= {props.date} />
       <div className = "expense-item__description">
-        <h2> {title} </h2>
+        <h2> {props.title} </h2>
         <div className= "expense-item__price"> ${props.amount} </div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
